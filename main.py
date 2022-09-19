04da916089743fcb8e991e0107c241424caf5483
Buddha Shrestha <buddha.shrestha1632@gmail.com>
2022-09-19 10:49:07 +0545

    added into the class

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 
-driver=webdriver.firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-element = driver.find_element(By.NAME, "q")
-element.clear()
-element.send.keys("pycon")
-element.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+class open_URL():
+    driver=webdriver.firefox()
+    driver.get("http://www.python.org")
+    assert "Python" in driver.title
+    element = driver.find_element(By.NAME, "q")
+    element.clear()
+    element.send.keys("pycon")
+    element.send_keys(Keys.RETURN)
+    assert "No results found." not in driver.page_source
+    driver.close()
 
 
 

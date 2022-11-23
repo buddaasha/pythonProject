633b2f8d4d0013608b232931dd4f6713ff303a75
buddaasha <buddaasha510@gmail.com>
2022-11-23 14:48:47 +0545

    local changes

--- self_test.py
+++ self_test.py
@@ -1,6 +1,19 @@
-class check:
+class check():
     def __init__(self):
         print("Address of self = ",id(self))
   
 obj = check()
 print("Address of class object = ",id(obj))
+
+class car():
+    def __init__(self,model,color):
+        self.model = model
+        self.color = color
+    
+    def show(self):
+        print("Model is", self.model)
+        print("color is",self.color)
+
+
+
+
